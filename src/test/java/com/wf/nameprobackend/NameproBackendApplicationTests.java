b8c8c7444bdb79598372d3e90f9d8200dce7aff7
SaiKrishna Teja <saikrishnatej@outlook.in>
2022-05-17 00:41:21 +0530

    maven update

--- src/test/java/com/wf/nameprobackend/NameproBackendApplicationTests.java
+++ src/test/java/com/wf/nameprobackend/NameproBackendApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class NameproBackendApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
